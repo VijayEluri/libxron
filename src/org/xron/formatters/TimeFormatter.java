22f888341452f45f4554393bbe3da568fd484e08
Ben Page [w] <benpage26@gmail.com>
2009-12-16 14:43:57 +0000

    Added TimeFormatter and formatToHHMMSS

---
+++ src/org/xron/formatters/TimeFormatter.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.xron.formatters;
+
+/**
+ *
+ * @author ben
+ */
+public class TimeFormatter {
+
+    public static void main(String[] args) {
+        testFormatToHHMMSS();
+    }
+
+    public static String formatToHHMMSS(int secsIn) {
+        int hours = secsIn / 3600,
+                remainder = secsIn % 3600,
+                minutes = remainder / 60,
+                seconds = remainder % 60;
+
+        return ((hours < 10 ? "0" : "") + hours + ":" + (minutes < 10 ? "0" : "") + minutes + ":" + (seconds < 10 ? "0" : "") + seconds);
+    }
+
+    public static void testFormatToHHMMSS(){
+        System.out.println(TimeFormatter.formatToHHMMSS(5));
+        System.out.println(TimeFormatter.formatToHHMMSS(60));
+        System.out.println(TimeFormatter.formatToHHMMSS(65));
+        System.out.println(TimeFormatter.formatToHHMMSS(3600));
+        System.out.println(TimeFormatter.formatToHHMMSS(3750));
+    }
+}
