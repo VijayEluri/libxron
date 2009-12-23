f0c468fe5c5a34901a9f17611eea848b3aee1d2d
Ben Page [w] <benpage26@gmail.com>
2009-12-23 23:35:46 +0000

    added Point3D, which may later be renamed to Vector3D

---
+++ src/org/xron/math/Point3D.java
@@ -0,0 +1,27 @@
+package org.xron.math;
+
+/**
+ *
+ * @author ben
+ */
+public class Point3D {
+
+
+    public double x;
+    public double y;
+    public double z;
+
+    public Point3D() {
+        this.x = 0;
+        this.y = 0;
+        this.z = 0;
+    }
+
+
+
+    public Point3D(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
