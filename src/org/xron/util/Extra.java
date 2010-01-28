b317d9e68c5ce9872e33cf6333c1ca9d9175966d
Ben Page [w] <benpage26@gmail.com>
2010-01-28 00:04:55 +0000

    Added Extra class with the sleep2 method.
    sleep2 is like sleep but doesn't throw exceptions, mainly for prototyping.

---
+++ src/org/xron/util/Extra.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.xron.util;
+
+/**
+ *
+ * @author Ben Page
+ */
+public class Extra {
+
+    /**
+     * Wrapper around Java's Thread.sleep(), ignores the exception thrown.
+     *
+     * @param      millis   the length of time to sleep in milliseconds.
+     * @see        java.lang.Thread#sleep(long)
+     */
+    public static void sleep2(long millis){
+        try{
+            Thread.sleep(millis);
+        } catch (InterruptedException x){
+            System.err.println("WARNING: Ignored exception in org.xron.util.Extra.sleep");
+        }
+    }
+
+}
