2ff4ee634d8ef7bfe319fe3e45465950076057a8
Ben Page [w] <benpage26@gmail.com>
2010-02-08 23:26:49 +0000

    Added MinMax, added to ArrayPrinter

--- src/org/xron/debug/ArrayPrinter.java
+++ src/org/xron/debug/ArrayPrinter.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package org.xron.debug;
 
 import java.io.PrintStream;
@@ -12,49 +11,72 @@ import java.io.PrintStream;
  * @author ben
  */
 public class ArrayPrinter {
-    
+
     static PrintStream out = System.out;
 
-    public static void printFloat(float[] arr){
-        out.println(arr.length + " elements:");
-        for (int i = 0; i < arr.length; i++) {
-            out.println("\t"+i+": "+arr[i]);
+    public static void printInt(int[] arr) {
+        if (arr == null) {
+            System.out.println("null");
+        } else {
+            out.println(arr.length + " elements:");
+            for (int i = 0; i < arr.length; i++) {
+                out.println("\t" + i + ": " + arr[i]);
+            }
         }
     }
 
-    public static void printFloats(float... arr){
+    public static void printInts(int... arr) {
         out.println(arr.length + " elements:");
         for (int i = 0; i < arr.length; i++) {
-            out.println("\t"+i+": "+arr[i]);
+            out.println("\t" + i + ": " + arr[i]);
         }
     }
 
-    public static void printObject(Object[] arr){
-        out.println(arr.length + " elements:");
-        for (int i = 0; i < arr.length; i++) {
-            out.println("\t"+i+": "+arr[i]);
+    public static void printFloat(float[] arr) {
+        if (arr == null) {
+            System.out.println("null");
+        } else {
+            out.println(arr.length + " elements:");
+            for (int i = 0; i < arr.length; i++) {
+                out.println("\t" + i + ": " + arr[i]);
+            }
         }
     }
 
-    public static void printObjects(Object... arr){
+    public static void printFloats(float... arr) {
         out.println(arr.length + " elements:");
         for (int i = 0; i < arr.length; i++) {
-            out.println("\t"+i+": "+arr[i]);
+            out.println("\t" + i + ": " + arr[i]);
         }
     }
 
-    public static void useStdOut(){
+    public static void printObject(Object[] arr) {
+        if (arr == null) {
+            System.out.println("null");
+        } else {
+            out.println(arr.length + " elements:");
+            for (int i = 0; i < arr.length; i++) {
+                out.println("\t" + i + ": " + arr[i]);
+            }
+        }
+    }
+
+    public static void printObjects(Object... arr) {
+        out.println(arr.length + " elements:");
+        for (int i = 0; i < arr.length; i++) {
+            out.println("\t" + i + ": " + arr[i]);
+        }
+    }
+
+    public static void useStdOut() {
         out = System.out;
     }
 
-    public static void useStdErr(){
+    public static void useStdErr() {
         out = System.err;
     }
 
-    public static void useStream(PrintStream x){
+    public static void useStream(PrintStream x) {
         out = x;
     }
-
-
-
 }
