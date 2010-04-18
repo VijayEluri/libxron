981d0fbb75e2168bf9a8a3d521ddd9aa8de85b79
Ben Page [w] <benpage26@gmail.com>
2010-04-18 22:51:09 +0100

    Autoboxing experiment

---
+++ src/org/xron/sandbox/AutoboxTest.java
@@ -0,0 +1,36 @@
+// Author: Ben Page [ug73bxp] 
+package org.xron.sandbox;
+
+import java.util.ArrayList;
+import java.util.List;
+import sun.swing.PrintColorUIResource;
+
+/**
+ *
+ * @author Ben Page [ug73bxp@cs.bham.ac.uk]
+ */
+public class AutoboxTest {
+
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        list.add(2);
+        print(list);
+        list.remove(2);
+        print(list);
+        Integer i = new Integer(40);
+        list.remove(i);
+        print(list);
+    }
+
+    static void print(List l){
+        System.out.println("----");
+        for (Object o : l) {
+            System.out.println(o);
+        }
+    }
+
+}
