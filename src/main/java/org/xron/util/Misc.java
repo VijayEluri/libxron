3378b161f7fc71a305e50de709ae012ac20e07ab
Ben Page <benpage26@gmail.com>
2016-05-13 15:07:23 +0100

    mapTheValues

--- src/main/java/org/xron/util/Misc.java
+++ src/main/java/org/xron/util/Misc.java
@@ -4,6 +4,10 @@
  */
 package org.xron.util;
 
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 /**
  * @author Ben Page
  */
@@ -22,4 +26,14 @@ public class Misc {
       System.err.println("WARNING: Ignored InterruptedException in libxron:sleep2");
     }
   }
+
+  /**
+   * apply map to the _value_ of an entry in a map, instead of the entry
+   *
+   * return map of key to the new value
+   */
+  private <K, V, O> Map<K, O> mapTheValues(Map<K, V> map, Function<V, O> funct) {
+    return map.entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey, e -> funct.apply(e.getValue())));
+  }
+
 }
